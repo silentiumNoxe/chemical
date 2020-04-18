e5a2b0d63c2980564b03d82a0bdea49cde20d35b
silentium_noxe <developer.silentium@gmail.com>
2020-04-18 17:38:44 +0300

    save camera logic

--- js/camera.js
+++ js/camera.js
@@ -1,5 +1,30 @@
 function Camera(canvasElement){
     this.x = 0;
     this.y = 0;
-    this.canvas = canvasElement;
+    /** @type HTMLCanvasElement*/this.canvas = canvasElement;
+
+    this.canvas.onmousemove = function (event) {
+        if(!this.clickStarted){
+            return;
+        }
+
+        let rect = this.getBoundingClientRect();
+        let mouse = {x: (event.clientX - rect.left).toFixed(0), y: (event.clientY - rect.top).toFixed(0)};
+        this.mouseEnd = {x: mouse.x - this.mouseStart.x, y: mouse.y - this.mouseStart.y};
+        this.camera.x = this.mouseEnd.x;
+        this.camera.y = this.mouseEnd.y;
+    };
+
+    this.canvas.onmousedown = function (event) {
+        let rect = this.getBoundingClientRect();
+        this.mouseStart = {
+            x: ((event.clientX - rect.left).toFixed(0)) - this.camera.x,
+            y: ((event.clientY - rect.top).toFixed(0)) - this.camera.y
+        };
+        this.clickStarted = true;
+    };
+
+    this.canvas.onmouseup = this.canvas.onmouseleave = function () {
+        this.clickStarted = false;
+    };
 }
--- js/index.js
+++ js/index.js
@@ -1,9 +1,6 @@
 window.addEventListener("DOMContentLoaded", () => {
     let canvasElement = document.querySelector("canvas");
-    canvasElement.onmousemove = canvasMoveListener;
-    canvasElement.onmousedown = canvasClickDownListener;
-    canvasElement.onmouseup = canvasClickUpListener;
-    canvasElement.camera = new Camera();
+    canvasElement.camera = new Camera(canvasElement);
 
     let canvas = canvasElement.getContext("2d");
     let timeElement = document.querySelector("#time");
@@ -20,41 +17,4 @@ window.addEventListener("DOMContentLoaded", () => {
         timeElement.innerHTML = String((performance.now() - startTime).toFixed(0)) + " ms";
     }, 10);
 });
-
-function canvasMoveListener(event) {
-    if(!this.clickStarted){
-        return;
-    }
-
-    let rect = this.getBoundingClientRect();
-
-    let mouse = {x: (event.clientX - rect.left).toFixed(0), y: (event.clientY - rect.top).toFixed(0)};
-    this.mouseEnd = {x: mouse.x - this.mouseStart.x, y: mouse.y - this.mouseStart.y};
-    this.camera.x = this.mouseEnd.x;
-    this.camera.y = this.mouseEnd.y;
-
-    document.querySelector("#mouse #asdf").innerHTML = "move.mouse "+JSON.stringify(mouse);
-    document.querySelector("#mouse #final").innerHTML = "move.result "+JSON.stringify(this.camera);
-}
-
-function canvasClickDownListener(event) {
-    let rect = this.getBoundingClientRect();
-    let click = {
-        x: (event.clientX - rect.left).toFixed(0),
-        y: (event.clientY - rect.top).toFixed(0)
-    };
-    this.mouseStart = {
-        x: ((event.clientX - rect.left).toFixed(0)) - this.camera.x,
-        y: ((event.clientY - rect.top).toFixed(0)) - this.camera.y
-    };
-
-    document.querySelector("#mouse #click").innerHTML = "click "+JSON.stringify(click);
-    document.querySelector("#mouse #camera").innerHTML = "click.camera "+JSON.stringify(this.camera);
-    document.querySelector("#mouse #result").innerHTML = "click.resultStart"+JSON.stringify(this.mouseStart);
-    this.clickStarted = true;
-}
-
-function canvasClickUpListener() {
-    this.clickStarted = false;
-}
 
