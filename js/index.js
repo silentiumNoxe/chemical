98ee4adc8e309e4182007cf7cdabb5d48f504073
silentium_noxe <developer.silentium@gmail.com>
2020-04-20 13:43:17 +0300

    save commit

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
 window.FBFMode = false;//frame-by-frame
+window.border = {x: 2000, y: 2000};
 
 window.addEventListener("DOMContentLoaded", () => {
     let canvasElement = document.querySelector("canvas");
@@ -8,10 +9,11 @@ window.addEventListener("DOMContentLoaded", () => {
     window.camera = new Camera(canvasElement);
     window.camera.scale = 10;
 
-    /** @type Entity[]*/window.entityArray = factoryEntities([
-        {name: "H", radius: 10, x: 100, y: 100, backgroundColor: "#bb0300", speed: 1, angle: 57},
-        {x: 150, speed: 1, angle: 90}
-    ]);
+    window.entityArray = [];
+    // /** @type Entity[]*/window.entityArray = factoryEntities([
+    //     {name: "H", radius: 20, x: 200, y: 200, backgroundColor: "white", speed: 2, angle: 60},
+    //     {x: 250, angle: 90}
+    // ]);
 
     window.requestAnimationFrame(loop);
 });
@@ -31,8 +33,8 @@ function loop() {
     canvas.font = 10*scale+"px Arial";
     canvas.textAlign = "center";
 
-    for(let i = 0; i < entityArray.length; i++){
-        /** @type Entity*/let entity = entityArray[i];
+    for(let i = 0; i < window.entityArray.length; i++){
+        /** @type Entity*/let entity = window.entityArray[i];
         // let previousEntity = i-1 < 0 ? entityArray[entityArray.length - 1] : entityArray[i-1];
 
         canvas.beginPath();
@@ -41,6 +43,8 @@ function loop() {
         canvas.arc((entity.x+window.camera.x) * scale, (entity.y+window.camera.y) * scale, entity.radius*scale, 0, 2 * Math.PI);
         if(entity.backgroundColor != null){
             canvas.fillStyle = canvas.strokeStyle = entity.backgroundColor;
+            if(canvas.strokeStyle === "#fff" || canvas.strokeStyle === "#ffffff" || canvas.strokeStyle === "white")
+                canvas.strokeStyle = "#000";
             canvas.fill();
         }
 
@@ -54,11 +58,15 @@ function loop() {
         canvas.stroke();
 
         entity.move();
-        entity.angle += 5;
-        if(entity.angle > 360){
-            entity.angle = 0;
+
+        if(entity.x > window.border.x || entity.y > window.border.y){
+            entityArray.splice(i, 1);
         }
+
+        entity.setAngle(entity.angle+2);
     }
+
+    collide();
 
     //debug information
     let fps = window.FBFMode ? "FBF" : (1000/(startTime-window.lastLoopTime)).toFixed(0);
@@ -69,10 +77,97 @@ function loop() {
     innerHTML("#camera").set(`camera x: ${camera.x} y: ${camera.y} scale: ${camera.scale}`);
     innerHTML("#entities").set("entities: "+window.entityArray.length);
 
+    // innerHTML("#en1").set(JSON.stringify(entityArray[0]));
+    // innerHTML("#en2").set(JSON.stringify(entityArray[1]));
+
+    generateRandomEntities(500);
+
     //next frame
     if(!window.FBFMode) {
         window.requestAnimationFrame(loop);
     }
+}
+
+function generateRandomEntities(max) {
+    while (window.entityArray.length < max){
+        let entity = new Entity();
+        entity.x = (Math.random() * window.border.x);
+        entity.y = (Math.random() * window.border.y);
+        entity.backgroundColor = "white";
+        entity.speed = Math.random() * 10;
+        entity.angle = Math.random() * 360;
+        entity.radius = Math.random() * 30;
+        window.entityArray.push(entity);
+    }
+}
+
+/**
+ * @param en1 {Entity}
+ * @param en2 {Entity}
+ * */
+function hitTestEntity(en1, en2) {
+    let dx = en1.nextX - en2.nextX;
+    let dy = en1.nextY - en2.nextY;
+    let distance = (dx*dx + dy*dy);
+
+    let res = distance <= Math.pow(en1.radius + en2.radius, 2);
+    // console.log(res, distance, Math.pow(en1.radius + en2.radius, 2));
+    // innerHTML("#collision").set("dis: "+distance+" a: "+Math.pow(en1.radius + en2.radius, 2)+" res: "+res);
+    return res;
+}
+
+function collide() {
+    for(let i = 0; i < entityArray.length; i++){
+        let entity = entityArray[i];
+        for(let k = 0; k < entityArray.length; k++){
+            let testEntity = entityArray[k];
+            if(hitTestEntity(entity, testEntity)){
+                collideEntities(entity, testEntity);
+            }
+        }
+    }
+}
+
+function collideEntities(en1, en2) {
+    let dx = en1.nextX - en2.nextX;
+    let dy = en1.nextY - en2.nextY;
+
+    let collisionAngle = Math.atan2(dy, dx);
+
+    let speed1 = Math.sqrt(en1.velocityX * en1.velocityX +
+        en1.velocityY * en1.velocityY);
+    let speed2 = Math.sqrt(en2.velocityX * en2.velocityX +
+        en2.velocityY * en2.velocityY);
+
+    let direction1 = Math.atan2(en1.velocityY, en1.velocityX);
+    let direction2 = Math.atan2(en2.velocityY, en2.velocityX);
+
+    let velocityx_1 = speed1 * Math.cos(direction1 - collisionAngle);
+    let velocityy_1 = speed1 * Math.sin(direction1 - collisionAngle);
+    let velocityx_2 = speed2 * Math.cos(direction2 - collisionAngle);
+    let velocityy_2 = speed2 * Math.sin(direction2 - collisionAngle);
+
+    let final_velocityx_1 = ((en1.mass - en2.mass) * velocityx_1 +
+        (en2.mass + en2.mass) * velocityx_2)/(en1.mass + en2.mass);
+    let final_velocityx_2 = ((en1.mass + en1.mass) * velocityx_1 +
+        (en2.mass - en1.mass) * velocityx_2)/(en1.mass + en2.mass);
+
+    let final_velocityy_1 = velocityy_1;
+    let final_velocityy_2 = velocityy_2;
+
+    en1.velocityX = Math.cos(collisionAngle) * final_velocityx_1 +
+        Math.cos(collisionAngle + Math.PI/2) * final_velocityy_1;
+    en1.velocityY = Math.sin(collisionAngle) * final_velocityx_1 +
+        Math.sin(collisionAngle + Math.PI/2) * final_velocityy_1;
+    en2.velocityX = Math.cos(collisionAngle) * final_velocityx_2 +
+        Math.cos(collisionAngle + Math.PI/2) * final_velocityy_2;
+    en2.velocityY = Math.sin(collisionAngle) * final_velocityx_2 +
+        Math.sin(collisionAngle + Math.PI/2) * final_velocityy_2;
+
+    en1.setNextX(en1.nextX += en1.velocityX);
+    en1.setNextY(en1.nextY += en1.velocityY);
+    en2.setNextX(en2.nextX += en2.velocityX);
+    en2.setNextY(en2.nextY += en2.velocityY);
 }
 
 function factoryEntities(entities) {
--- js/models/entity.js
+++ js/models/entity.js
@@ -1,9 +1,13 @@
 function Entity(name) {
     this.x = 0;
     this.y = 0;
+    this.nextX = this.x;
+    this.nextY = this.y;
+
     this.radius = 1;
     this.speed = 0;
     this.angle = 0;
+    this.mass = 1;
 
     this.name = name;
 
@@ -12,9 +16,46 @@ function Entity(name) {
 
 Entity.prototype.move = function () {
     let radians = this.angle * Math.PI / 180;
-    let xunits = Math.cos(radians) * this.speed;
-    let yunits = Math.sin(radians) * this.speed;
+    this.velocityX = Math.cos(radians) * this.speed;
+    this.velocityY = Math.sin(radians) * this.speed;
 
-    this.x += xunits;
-    this.y += yunits;
+    this.nextX = (this.x += this.velocityX);
+    this.nextY = (this.y += this.velocityY);
+};
+
+Entity.prototype.collideWith = function (target) {
+    let speed1 = Math.sqrt(Math.pow(this.velocityX, 2) + Math.pow(this.velocityY, 2));
+    let speed2 = Math.sqrt(Math.pow(target.velocityX, 2) + Math.pow(target.velocityY, 2));
+
+    let direction1 = Math.atan2(this.velocityX, this.velocityY);
+    let direction2 = Math.atan2(target.velocityX, target.velocityY);
+};
+
+Entity.prototype.setX = function (val) {
+    this.x = Number(val).toFixed(0);
+};
+
+Entity.prototype.setY = function (val) {
+    this.y = Number(val).toFixed(0);
+};
+
+Entity.prototype.setNextX = function (val) {
+    this.nextX = Number(val).toFixed(0);
+};
+
+Entity.prototype.setNextY = function (val) {
+    this.nextY = Number(val).toFixed(0);
+};
+
+Entity.prototype.setAngle = function (val) {
+    let max = 360;
+    while (val > max) {
+        val -= max;
+    }
+
+    while (val < 0){
+        val += max;
+    }
+
+    this.angle = val;
 };
